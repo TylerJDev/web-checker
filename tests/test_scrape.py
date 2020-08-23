c1e2f8bd436c9436593f06e534388ef67410fd31
Tyler <tylerjdev@gmail.com>
2020-08-23 11:56:05 -0400

    Added more tests

--- tests/test_scrape.py
+++ tests/test_scrape.py
@@ -2,8 +2,15 @@ import unittest
 from main import grabItemRequest
 
 class TestMyScraper(unittest.TestCase):
-    def test_equalToTwo(self):
-        page = 'https://tylerjdev.github.io/web-checker/'
-        self.assertEqual(grabItemRequest(page, 'tagName', 'h1'), 'Hello, World!')
+    def setUp(self): 
+        self.def_page = 'https://tylerjdev.github.io/web-checker/'
+    
+    def test_equalToTagNameText(self):
+        self.assertEqual(grabItemRequest(self.def_page, 'tagName', 'h1'), 'Hello, World!')
 
+    def test_equalToIDNameText(self):
+        self.assertEqual(grabItemRequest(self.def_page, 'id', 'test_id'), 'Hello, World, too!')
+        
+    def test_equalToClassNameText(self):
+        self.assertEqual(grabItemRequest(self.def_page, 'className', 'test_class'), 'Drive in, drive in!')
 
